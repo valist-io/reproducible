344ab128411d9a6b3b9c772d3cbaf7de6dcdfcc2
Zachary Pelkey <zpelkey@gmail.com>
2021-06-24 13:57:56 -0400

    Added basic tests

---
+++ tests/reproducible.test.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+const reproducible = require('..');
+
+describe('Test Valist Reproducible', async () => {
+  describe('Create Build Image', async () => {
+    it('Should return true', async () => {
+      expect(reproducible.createImage());
+    });
+  });
+
+  describe('Create Build Image', async () => {
+    it('Should return true', async () => {
+      expect(await reproducible.createBuildEnv({
+        image: 'valist-build',
+        command: ['cp', '/opt/valist/hello', '/opt/valist/dist'],
+        source: `${process.cwd()}/dist`,
+      }));
+    });
+  });
+});
