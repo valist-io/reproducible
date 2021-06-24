94b60b0f5dd376fa17273d927ebcc6e99d6495e7
Zachary Pelkey <zpelkey@gmail.com>
2021-06-24 13:30:51 -0400

    Added logging of container image build steps to stdout

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,45 @@
 const Docker = require('dockerode');
 const tar = require('tar-fs');
 const path = require('path');
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
 export const createImage = async (dockerFilePath?: string) => {
-  const context = dockerFilePath ? path.dirname(dockerFilePath): process.cwd();
+  const context = dockerFilePath ? path.dirname(dockerFilePath) : process.cwd();
   const tarStream = tar.pack(context);
-  const image = await docker.buildImage(tarStream, { t: 'valist-build' });
+  const imageStream = await docker.buildImage(tarStream, { t: 'valist-build' });
 
   const buildLog = await new Promise((resolve, reject) => {
-    docker.modem.followProgress(image, (err: any, res: any) => {
+    // Log the container build steps
+    imageStream.pipe(process.stdout);
+
+    docker.modem.followProgress(imageStream, (err: any, res: any) => {
       if (err) {
-        reject(err)
+        reject(err);
       }
+
       resolve(res);
     });
-    // @TODO log the container build steps
   });
-  console.log(buildLog);
-}
 
-export const createBuildEnv = async ({ image, command, source, artifact }: any) => {
+  console.log('Build has completed!');
+  return buildLog;
+};
+
+export const createBuildEnv = async ({ image, command, source }: any) => {
   const container = await docker.createContainer({
     Image: image,
     Cmd: command,
     HostConfig: {
       AutoRemove: true,
       Mounts: [{
-        Target:   '/opt/valist/dist',
-        Source:   source,
-        Type:     'bind',
-        ReadOnly: false
-      }]
-    }
+        Target: '/opt/valist/dist',
+        Source: source,
+        Type: 'bind',
+        ReadOnly: false,
+      }],
+    },
   });
 
   container.start();
-}
+};
