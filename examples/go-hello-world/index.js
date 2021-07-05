c1a4931d973d3db22ac5b1413cc559a6839a61d7
Zachary Pelkey <zpelkey@gmail.com>
2021-07-05 14:56:38 -0400

    Added support for custom dockerfile

--- examples/go-hello-world/index.js
+++ examples/go-hello-world/index.js
@@ -3,5 +3,5 @@ const reproducible = require('../../');
 (async () => {
   await reproducible.generateDockerfile('golang:buster', './', 'go build -o ./dist/main src/main.go');
   await reproducible.createBuild('valist-build');
-  await reproducible.exportBuild('valist-build','dist/main');
+  await reproducible.exportBuild('valist-build', 'dist/main');
 })();
